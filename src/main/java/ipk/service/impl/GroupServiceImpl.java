a9c4faa5d91ec732e83c61359ef1fe94cf91f4e6
Nastia Fasova <fasovaaa@gmail.com>
2020-12-13 17:19:06 +0200

    Edited deleting of listeners of group

--- src/main/java/ipk/service/impl/GroupServiceImpl.java
+++ src/main/java/ipk/service/impl/GroupServiceImpl.java
@@ -56,6 +56,7 @@ public class GroupServiceImpl implements GroupService {
     public Group updateListener(Listener listener, Group group) {
         Listener listenerByEmail = listenerService.getByEmail(listener.getEmail());
         group.getListeners().remove(listenerByEmail);
+        listenerService.deleteById(listenerByEmail.getId());
         Set<Role> roles = listener.getRoles();
         listenerByEmail.setRoles(roles);
         listenerByEmail.setName(listener.getName());
@@ -72,6 +73,7 @@ public class GroupServiceImpl implements GroupService {
     public Group deleteListenerFromGroup(Listener listener, Long id) {
         Group group = groupRepository.findById(id).orElseThrow(RuntimeException::new);
         group.getListeners().remove(listener);
+        listenerService.deleteById(listener.getId());
         groupRepository.save(group);
         return group;
     }
